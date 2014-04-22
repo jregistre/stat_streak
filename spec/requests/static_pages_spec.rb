8c228a7821bc1a25a32be64f2c21a24887d9a794
Jerel Registre <jregistre@gmail.com>
2014-04-21 22:11:02 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
+describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Stat Streak'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Stat Streak')
-    end
+    before { visit root_path }
+
+    it { should have_content('Stat Streak') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
- 
+
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
-  
-  describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-    
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Stat Streak | About Us")
-    end
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
